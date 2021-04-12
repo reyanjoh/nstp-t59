c0fa44e7099c93f9d185e3b816b1509b87b204ec
Reyan John Narvaja <73632471+reyanjoh@users.noreply.github.com>
2021-04-13 06:04:03 +0800

    toggle

--- assets/js/modules/clicksOnly.js
+++ assets/js/modules/clicksOnly.js
@@ -19,19 +19,39 @@ export const Showmore = (Blogs) => {
 
 export const toggleBtn = () => {
     let tgle = true;
-    const toggleBtn = document.querySelector('.toggle-btn');
-    const sideNav = document.querySelector('.mobile-side-nav-filter');
+    const toggleBtn = document.querySelectorAll('.toggle-btn');
+    const sideNavFilter = document.querySelector('.mobile-side-nav-filter');
     const mobileSideNav = document.querySelector('.mobile-side-nav');
     const blogPage = document.querySelector('.blog-page');
     
 
 
-    toggleBtn.addEventListener('click', (e) => {
+    toggleBtn[0].addEventListener('click', (e) => {
         tgle = !tgle;
-        toggleBtn.style = 'display: hidden';
-        sideNav.style = 'display: unset';
-        blogPage.style = 'opacity: .4';
-        mobileSideNav.style = 'margin-right: 0';
-        console.log(tgle);
+        // toggleBtn.style = 'display: hidden';
+        if( !tgle ){
+            sideNavFilter.style = 'display: unset';
+            blogPage.style = 'opacity: .4';
+            mobileSideNav.style = 'margin-right: 0';
+            console.log(tgle);
+        }
+        
     });
+
+    sideNavFilter.addEventListener('click', (e) => {
+        if(e.target.classList == 'mobile-side-nav-filter'){
+            tgle = !tgle;
+        console.log(e.target);
+        // toggleBtn.style = 'display: hidden';
+            if( tgle ){
+                sideNavFilter.style = 'display: none';
+                blogPage.style = 'opacity: 1';
+                mobileSideNav.style = 'margin-right: -30rem';
+                console.log(tgle);
+            }
+        }
+        
+    });
+
+
 }
