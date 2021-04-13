af9a0924f37f68f62328cdf1a029a7d78c7b7277
Reyan John Narvaja <73632471+reyanjoh@users.noreply.github.com>
2021-04-14 03:10:22 +0800

    Delete nstp-t59 v2 directory

--- nstp-t59 v2/assets/js/modules/blogsDOM.js
+++
@@ -1,7 +0,0 @@
-  
-export const HideBlogArticlePContent = () => {
-    const articleP = document.querySelectorAll('.article-p');
-    for (let i = 0; i < articleP.length; i++) {
-        console.log(articleP[i].innerText.substring(0,50));
-    }
-}
